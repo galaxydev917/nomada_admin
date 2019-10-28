065f42bcdef2b80bd8d1688fc5791f27027d4133
Ismail-PC\Ismail <engr.mohsin14@gmail.com>
2019-10-28 17:38:22 +0500

    new version with fcm

--- resources/views/menuList.blade.php
+++ resources/views/menuList.blade.php
@@ -81,7 +81,7 @@
 <script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
 <script src="https://www.gstatic.com/firebasejs/5.10.1/firebase.js"></script>
 <script>
-    Initialize Firebase
+    //Initialize Firebase
     var config = {
         apiKey: "{{ config('services.firebase.api_key') }}",
         authDomain: "{{ config('services.firebase.auth_domain') }}",
